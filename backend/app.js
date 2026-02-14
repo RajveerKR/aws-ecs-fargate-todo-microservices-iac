df440aa91a4817bf645cb25467c9c300f5adb7f6
Rajveer KR <razzi.khushi@gmail.com>
2026-02-14 15:32:37 +1300

    Add backend and frontend containerized services

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let todos = [{ id: 1, task: "Learn Docker" }];
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.post("/todos", (req, res) => {
+  const newTodo = {
+    id: todos.length + 1,
+    task: req.body.task || "New Task"
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
